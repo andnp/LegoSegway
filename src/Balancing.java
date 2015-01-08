d0f827a01d92816c800018f49367b8470073edca
andnp <soundguyandyp@gmail.com>
2015-01-08 12:01:13 -0500

    Removed multi-threading

--- src/Balancing.java
+++ src/Balancing.java
@@ -5,17 +5,12 @@ import nl.totan.sensors.*;
 
 public class Balancing extends Thread{
 	public static float MAX_SPEED = Motor.A.getMaxSpeed();
-	private Attitude attitude;
-	
-	Balancing(Attitude attitude){
-		this.attitude = attitude;
-	}
 	
 	public void run(){
-		attitude.start();
 		
-		while(attitude.isStarting()){
-		}
+		SensorPort.S4.i2cEnable(I2CPort.HIGH_SPEED);
+		//MMA7455L accel = new MMA7455L(SensorPort.S4);
+		L3G4200D gyro = new L3G4200D(SensorPort.S4);
 		
 //		float[] startingTilt = getAverageTilt(accel, 15); // Find the starting angle between bot and ground
 //		float[] startingRate = getAverageRate(gyro, 15);
@@ -26,25 +21,35 @@ public class Balancing extends Thread{
 		float derivative_error = 0; // Error based on the Derivative part of the PID
 		float raw_gyro = 0;
 		float estimated_tilt = 0;
+		float dt = 0; // change in time
+		long start_time = 0;
+		
+//		float[] tilt = new float[3]; // array containing {x_tilt,y_tilt,z_tilt} in degrees
+		float[] rate = new float[3]; // array containing {x_rate,y_rate,z_rate} in degrees/second
 
-		float kp = (float) .06; // Gain (constant) of the proportional portion of the PID
+		float kp = (float) .50; // Gain (constant) of the proportional portion of the PID
 		float kd = (float) .0; // Gain (constant) of the proportional portion of the PID
 		
 		while(Button.ENTER.isUp()){ // controller loop
-			raw_gyro = Attitude.getRawGyro();
-			estimated_tilt = Attitude.getEstimatedTilt();
+			start_time = System.nanoTime();
+			
+			gyro.fetchAllRate(rate);  // update rate array
+//			accel.fetchAllTilt(tilt); // update tilt array
+			
+//			raw_tilt = tilt[0]; // Read the raw x compenent of the accelerometer in degrees
+			raw_gyro = rate[0]; // Read the raw x component of the gyroscope in degrees/sec
 			
 			derivative_error = raw_gyro;
 			proportional_error = estimated_tilt;
 			
 			error = kp * proportional_error + kd * derivative_error;
 			
-			RConsole.println("x_tilt:" + estimated_tilt + "   x_rate:" + raw_gyro);
+			//RConsole.println("x_tilt:" + estimated_tilt + "   x_rate:" + raw_gyro);
 			setMotorSpeed(error);
-			
-			
+			dt = (long) (System.nanoTime() - start_time) / 1000000; // change in time in milliseconds
+			estimated_tilt += rate[0] * (dt / 1000);
+			RConsole.println("dt: " + dt);
 		}
-		attitude.killThread();
 	}
 	
 	private static float[] getAverageTilt(MMA7455L accel, int trials){
@@ -76,7 +81,7 @@ public class Balancing extends Thread{
 				Motor.A.forward();
 				Motor.B.forward();
 			}
-			RConsole.println("motor_speed: " + motor_speed);
+			//RConsole.println("motor_speed: " + motor_speed);
 		}
 	}
 	
--- src/SegwayControl.java
+++ src/SegwayControl.java
@@ -2,8 +2,7 @@ import lejos.nxt.comm.RConsole;
 
 
 public class SegwayControl {
-	private static Attitude attitude = new Attitude();
-	private static Balancing balancer = new Balancing(attitude);
+	private static Balancing balancer = new Balancing();
 	public static void main(String[] args){
 		RConsole.openBluetooth(10000); // open Remote Console
 		balancer.start();
